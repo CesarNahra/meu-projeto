cafd11066b6a15457285aa9774f28a3bc7057c32
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-06-17 10:25:08 -0300

    Continuação props

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import googleLogo from '../img/googlelogo.png';
 import '../styles/Main.css'
 import Button from './Button.js';
+import Lupa from '../img/lupa.png';
 
 function Main() {
     return (  
@@ -11,8 +12,8 @@ function Main() {
                 <br />
                 <input type="text" name="" id="" />
                 <br />
-                <Button prop1='Pesquisa Google' prop2='Google search' prop3/>
-                <Button prop1='Estou com sorte' prop2='I"m lucky' prop3={false}/>
+                <Button prop1='Pesquisa Google' prop2={25} prop3/>
+                <Button prop1='Estou com sorte' prop2={30} prop3={false}/>
             </div>
         </React.Fragment>
     );
